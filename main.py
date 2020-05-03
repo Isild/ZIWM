f18df56423ef9583494b858f28429c5c29a3d7e9
lpawelcz <czarnecki.l.p@gmail.com>
2020-05-03 17:28:28 +0200

    formatting fix

--- main.py
+++ main.py
@@ -1,94 +1,97 @@
+import xlsxwriter
 import pandas
 import numpy as np
 import sklearn.feature_selection as feat_select
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.neural_network import MLPClassifier
-import xlsxwriter
 
 best_conf_matrix = [0, False, 0, np.ndarray, 0]
-
 
 def main():
     x, y = load_data()
     result = feature_selection(x, y)
+    new_list = result[1]
+
     print("dane:\n", x,y)
     print("========================\n")
     print((result[1]))
-    new_list = result[1]
     print("długości: ", len(x), len(y))
     print("Score`;\n", len(new_list))
-    
-    with xlsxwriter.Workbook('ranking.xlsx') as workbook:
-    
-        worksheet = workbook.add_worksheet()
 
+    with xlsxwriter.Workbook('ranking.xlsx') as workbook:
+        worksheet = workbook.add_worksheet()
         for row_num, data in enumerate(new_list):
             worksheet.write_row(row_num, 0, data)
-    
-    #layer_widths = [500, 900, 1200]
-    #momentum = [False, True]
-    #for width in layer_widths:
-        #print("Hidden layer width: " + str(width))
-        #for m in momentum:
-            #print("Momentum: " + str(m))
-            #train_evaluate(x, y, width, m)
-    #print("\n\n\nSUMMARY\n------------------------------------------\n")
-    #print("Hidden layer width: " + str(best_conf_matrix[0]) + "\nMomentum: " +
-          #str(best_conf_matrix[1]) + "\nFeatures number: " + str(best_conf_matrix[2]))
-    #print("Confusion matrix: ")
-    #print(best_conf_matrix[3])
-    #print("\nScore: " + str(best_conf_matrix[4]))
-    
-    #load_data()
+
+    # layer_widths = [500, 900, 1200]
+    # momentum = [False, True]
+    # for width in layer_widths:
+        # print("Hidden layer width: " + str(width))
+        # for m in momentum:
+            # print("Momentum: " + str(m))
+            # train_evaluate(x, y, width, m)
+    # print("\n\n\nSUMMARY\n------------------------------------------\n")
+    # print("Hidden layer width: " + str(best_conf_matrix[0]) + "\nMomentum: " +
+          # str(best_conf_matrix[1]) + "\nFeatures number: " + str(best_conf_matrix[2]))
+    # print("Confusion matrix: ")
+    # print(best_conf_matrix[3])
+    # print("\nScore: " + str(best_conf_matrix[4]))
+
+    # load_data()
 
 def load_data():
-    fileNames = ['ang_prct_2.txt', 'ang_prect.txt', 'inne.txt', 'mi_np.txt', 'mi.txt']
-    
+    fileNames = ['ang_prct_2.txt', 'ang_prect.txt', 'inne.txt',
+                                         'mi_np.txt', 'mi.txt']
+
     fiX = []
     fiY = []
     classNumber=1
-    
-    #pierwsze wczytanie danych z pliku, nie jest to w pętli, ponieważ był problem ze sklejaniem danych o różnych wymiarach, a nie wiem jak zrobić pustą tabele o konkretnych wymiarach
+
+    # pierwsze wczytanie danych z pliku, nie jest to w pętli,
+    # ponieważ był problem ze sklejaniem danych o różnych wymiarach,
+    # a nie wiem jak zrobić pustą tabele o konkretnych wymiarach
     dataframeX = pandas.read_csv('ang_prct_2.txt', sep="	", header=None)
     print("Wczytane dane:\n", dataframeX)
-    dataframeX=dataframeX.T
+    dataframeX = dataframeX.T
     print("Obrócone dane:\n", dataframeX)
-    
+
+    number = []
     cC = []
+    d1 = []
     x = []
     y = []
-    number = []
-    d1 = []
-    
+
     #kolumna z numerami
     for i in range(1, len(dataframeX)+1):
         cC.append(i)
         number.append(classNumber)
+
     #print(cC)
     dataframeX.insert(loc=0, column=-1, value=cC)
     print("Tablica z dodanym ponumerowaniem pacjentów:\n", dataframeX)
     dataframeX.insert(loc=0, column=-2, value=number)
-    
-    
+
     for f in fileNames:
         print("Plik: ", f)
         dataframe = pandas.read_csv(f, sep="	", header=None)
         #print("Wczytane dane:\n", dataframe)
         dataframe=dataframe.T
         #print("Obrócone dane:\n", dataframe)
-        
+
+        number = []
         cC = []
+        d1 = []
         x = []
         y = []
-        number = []
-        d1 = []
-        
+
         for i in range(1, len(dataframe)+1):
             cC.append(i)
             number.append(classNumber)
+
         #print(cC)
-        #wstawiam nową kolumnę z numerami, żeby format wporwadzonych danych się zgadzał
+        # wstawiam nową kolumnę z numerami,
+        # żeby format wporwadzonych danych się zgadzał
         dataframe.insert(loc=0, column=-1, value=cC)
         #print("Tablica z dodanym ponumerowaniem pacjentów:\n", dataframe)
         dataframe.insert(loc=0, column=-2, value=number)
@@ -98,22 +101,21 @@ def load_data():
         y = d1[:, 0]  # class column; kolumna 0 jako nr klas
 
         if classNumber != 1:
-            #sklejam ze sobą kolejne dane z pliku
+            # sklejam ze sobą kolejne dane z pliku
             dataframeX=dataframeX.append(dataframe, ignore_index=True)
-            
+
         classNumber+=1
-    
+
     print(dataframeX)
     d1 = dataframeX.to_numpy()
-    x = d1[:, 2:61]  # features columns; od:do kolumny
-    y = d1[:, 0]  # class column; kolumna 0 jako nr klas
+    x = d1[:, 2:61]	# features columns; od:do kolumny
+    y = d1[:, 0]	# class column; kolumna 0 jako nr klas
     print(x, y)
-    dataframeX.to_excel("output.xlsx", index=False) 
+    dataframeX.to_excel("output.xlsx", index=False)
     return x, y.astype(np.int)
 
-
 def feature_selection(x, y, n_best=59):
-    print("dl x:", (x), ", y:", (y)) 
+    print("dl x:", (x), ", y:", (y))
     selector = feat_select.SelectKBest(score_func=feat_select.chi2, k=n_best)
     fit = selector.fit(x, y)
     fit_x = selector.transform(x)
@@ -124,18 +126,20 @@ def feature_selection(x, y, n_best=59):
     print("Score wew:", len(scores))
     return fit_x, scores
 
-
 def train_evaluate(x, y, hidden_layer_width=900, momentum=True):
-    for i in range(1, 8): # 7 best features
+    for i in range(1, 8):	# 7 best features
         global best_conf_matrix
         fit_x, _ = feature_selection(x, y, i)
         kf = RepeatedStratifiedKFold(2, 5, random_state=42)
         if momentum:
-            mlp = MLPClassifier(hidden_layer_sizes=(hidden_layer_width,), max_iter=1000,
-                                nesterovs_momentum=True, solver='sgd', verbose=False, random_state=1)
+            mlp = MLPClassifier(hidden_layer_sizes=(hidden_layer_width,),
+                                max_iter=1000, nesterovs_momentum=True,
+                                solver='sgd', verbose=False, random_state=1)
         else:
-            mlp = MLPClassifier(hidden_layer_sizes=(hidden_layer_width,), max_iter=1000,
-                                solver='sgd', verbose=False, momentum=0, random_state=1)
+            mlp = MLPClassifier(hidden_layer_sizes=(hidden_layer_width,),
+                                max_iter=1000, solver='sgd', verbose=False,
+                                                 momentum=0, random_state=1)
+
         val_acc_features = []
 
         for train_index, test_index in kf.split(fit_x, y):
@@ -149,9 +153,10 @@ def train_evaluate(x, y, hidden_layer_width=900, momentum=True):
             s = mlp.score(x_test, y_test)
             if best_conf_matrix[4] < s:
                 best_conf_matrix = [hidden_layer_width, momentum, i, conf_mat, s]
-
             val_acc_features.append(s)
-        print("Mean score for feature: " + str(i) + " " + str(np.mean(val_acc_features)))
+
+        print("Mean score for feature: " + str(i) +
+              " " + str(np.mean(val_acc_features)))
 
 
 if __name__ == "__main__":
